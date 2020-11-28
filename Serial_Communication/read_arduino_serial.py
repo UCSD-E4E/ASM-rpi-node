5223567e1549ab6ed4b34c5de236b611346542ac
kymiyamo <59775899+kymiyamo@users.noreply.github.com>
2020-11-28 13:48:52 -0800

    Update read_arduino_serial.py

--- Serial_Communication/read_arduino_serial.py
+++ Serial_Communication/read_arduino_serial.py
@@ -2,18 +2,22 @@ import serial
 import datetime as dt
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 115200)
+    ser = serial.Serial('/dev/ttyACM0', 1000000)
     #ser.flush()
     
-    while True:
+    num_bytes = 0
+    total_time_s = 0
+    
+    while (total_time_s < 10):
         if ser.in_waiting > 0:
             start = dt.datetime.now()
-            line = ser.readline()
+            line = ser.read()
             end = dt.datetime.now()
-            print(line)
-            break
-    total_time_s = (end - start).total_seconds()
-    throughput = 100 / total_time_s
+            num_bytes += 1
+            total_time_s = total_time_s + (end - start).total_seconds()
+    throughput = num_bytes / total_time_s
+    print("Number of bytes")
+    print (num_bytes)
     print("time")
     print(total_time_s)
     print("Throughput is ")
