6b5071536fee6e82e7c9f523a15532c421e60b71
kymiyamo <59775899+kymiyamo@users.noreply.github.com>
2020-12-01 22:13:48 -0800

    added struct

--- Serial_Communication/AccGyrDataSerial.ino
+++ Serial_Communication/AccGyrDataSerial.ino
@@ -1,32 +1,43 @@
 #include <Arduino_LSM9DS1.h>
+
+struct IMUdata {
+	float acc_x;
+	float acc_y;
+	float acc_z;
+	float gyr_x:
+	float gyr_y;
+	float gyr_z;
+};
+
+IMUdata IMUdata;
+
 void setup() {
 	Serial.begin(9600);
 	IMU.begin();
 }
 
 void loop() {
-	float acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z;
 	if (IMU.accelerationAvailable()) {
-		IMU.readAcceleration(acc_x, acc_y, acc_z);
-		Serial.print("Accelerometer sample rate = ");
-		Serial.print(IMU.accelerationSampleRate());
+		IMU.readAcceleration(IMUdata.acc_x, IMUdata.acc_y, IMUdata.acc_z);
+		//Serial.print("Accelerometer sample rate = ");
+		//Serial.print(IMU.accelerationSampleRate());
 		Serial.println("Acc_x = ");
-		Serial.println(acc_x);
+		Serial.println(IMUdata.acc_x);
 		Serial.println("Acc_y = ");
-		Serial.println(acc_y);
+		Serial.println(IMUdata.acc_y);
 		Serial.println("Acc_z = ");
-		Serial.println(acc_z);
+		Serial.println(IMUdata.acc_z);
 	}
 
 	if (IMU.gyroscopeAvailable()) {
-		IMU.readGyroscope(gyr_x, gyr_y, gyr_z);
-		Serial.print("Gyrosccope sample rate = ");
-		Serial.print(IMU.gyroscopeSampleRate());
+		IMU.readGyroscope(IMUdata.gyr_x, IMUdata.gyr_y, IMUdata.gyr_z);
+		//Serial.print("Gyrosccope sample rate = ");
+		//Serial.print(IMU.gyroscopeSampleRate());
 		Serial.println("Gyr_x = ");
-		Serial.println(gyr_x);
+		Serial.println(IMUdata.gyr_x);
 		Serial.println("Gyr_y = ");
-		Serial.println(gyr_y);
+		Serial.println(IMUdata.gyr_y);
 		Serial.println("Gyr_z = ");
-		Serial.println(gyr_z);
+		Serial.println(IMUdata.gyr_z);
 	}
 }
