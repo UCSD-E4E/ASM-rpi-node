6611e779624e53722ceac573dbb1ba52f07baf5b
Nathan Hui <ntlhui@gmail.com>
2021-09-08 23:14:39 -0700

    Added PWM LED illuminator support

--- SensorNode/sensor_nodes/onBoxSensorNode.py
+++ SensorNode/sensor_nodes/onBoxSensorNode.py
@@ -1,13 +1,26 @@
 import asyncio
-from SensorNode import node
+import datetime as dt
+import sys
+from typing import Dict, Optional, Tuple, Type, Union
+
 from asm_protocol import codec
+from SensorNode import node
+
+try:
+    import gpiozero
+except ImportError:
+    print("Warning: PWM LED will not function!")
 
 
 class OnBoxSensorNode(node.SensorNodeBase):
     SENSOR_CLASS = 'ASM_NestingBox'
 
-    NESTING_BOX_KEYS = {
-        'video_endpoint': str
+    NESTING_BOX_KEYS: Dict[str, Union[Type, Tuple[Type, Type]]] = {
+        'video_endpoint': str,
+        'illumination_on': str,
+        'illumination_off': str,
+        'illumination_level': (int, float),
+        'illumination_pin': str
     }
 
     def __init__(self, config_path: str):
@@ -31,7 +44,42 @@ class OnBoxSensorNode(node.SensorNodeBase):
         self.registerPacketHandler(codec.E4E_START_RTP_RSP,
                                    self.onRTPCommandResponse)
 
+        self.led: Optional[gpiozero.PWMLED] = None
+        if 'gpiozero' in sys.modules:
+            pin = sensor_params['illumination_pin']
+            self.led = gpiozero.PWMLED(pin)
+            self.led_on: dt.time = dt.time.fromisoformat(
+                sensor_params['illumination_on'])
+            self.led_off: dt.time = dt.time.fromisoformat(
+                sensor_params['illumination_off'])
+            self.led_value: float = sensor_params['illumination_level'] / 100
+
+    async def turnLEDOn(self):
+        while self.led:
+            targetTime = dt.datetime.combine(dt.date.today(), self.led_on)
+            now = dt.datetime.now()
+            if targetTime < now:
+                targetTime += dt.timedelta(days=1)
+
+            sleep_time = (targetTime - now).total_seconds()
+            await asyncio.sleep(sleep_time)
+            self.led.value = self.led_value
+
+    async def turnLEDOff(self):
+        while self.led:
+            targetTime = dt.datetime.combine(dt.date.today(), self.led_off)
+            now = dt.datetime.now()
+            if targetTime < now:
+                targetTime += dt.timedelta(days=1)
+
+            sleep_time = (targetTime - now).total_seconds()
+            await asyncio.sleep(sleep_time)
+            self.led.value = 0
+
     async def setup(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.turnLEDOff())
+        loop.create_task(self.turnLEDOn())
         command = codec.E4E_START_RTP_CMD(self.uuid, self.data_server_uuid)
         await self.sendPacket(command)
         return await super().setup()
