1973265d7b2693ab8a24260bef0ae81e25d2b66b
Nathan Hui <ntlhui@gmail.com>
2021-09-08 19:27:16 -0700

    Added search paths for config file

--- runSensorNode.py
+++ runSensorNode.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
+import os
+
 from SensorNode.node import runSensorNode
-import logging
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    sensor_node = runSensorNode('test_config.yaml')
+    if os.path.isfile('/boot/asm_config.yaml'):
+        sensor_node = runSensorNode('/boot/asm_config.yaml')
+    elif os.path.isfile('/usr/local/etc/asm_config.yaml'):
+        sensor_node = runSensorNode('/usr/local/etc/asm_config.yaml')
+    else:
+        sensor_node = runSensorNode('asm_config.yaml')
     sensor_node.run()
