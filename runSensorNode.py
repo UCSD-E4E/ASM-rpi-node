24898bfe438c29078afcb1888026b15dfe8e24a4
Nathan Hui <ntlhui@gmail.com>
2021-10-12 00:01:55 -0700

    Renamed runSensorNode to createSensorNode

--- runSensorNode.py
+++ runSensorNode.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 import os
 
-from SensorNode.node import runSensorNode
+from SensorNode.node import createSensorNode
 
 if __name__ == '__main__':
     if os.path.isfile('/boot/asm_config.yaml'):
-        sensor_node = runSensorNode('/boot/asm_config.yaml')
+        sensor_node = createSensorNode('/boot/asm_config.yaml')
     elif os.path.isfile('/usr/local/etc/asm_config.yaml'):
-        sensor_node = runSensorNode('/usr/local/etc/asm_config.yaml')
+        sensor_node = createSensorNode('/usr/local/etc/asm_config.yaml')
     else:
-        sensor_node = runSensorNode('asm_config.yaml')
+        sensor_node = createSensorNode('asm_config.yaml')
     sensor_node.run()
