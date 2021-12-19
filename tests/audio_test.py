9dc12441b163f06133c8b6d745a5df0af1c0c8d5
Nathan Hui <ntlhui@gmail.com>
2021-12-18 23:00:39 -0800

    Updated test

--- tests/audio_test.py
+++ tests/audio_test.py
@@ -4,9 +4,10 @@ import SensorNode.utils.rtp as rtp
 import SensorNode.utils.ffmpeg as ffmpeg
 import subprocess
 import os
+import json
 
 def test_audio():
-    test_time = 30
+    test_time = 15
     if os.path.isfile('test.mp3'):
         os.remove('test.mp3')
     rx_ffmpeg = subprocess.Popen(
@@ -22,3 +23,10 @@ def test_audio():
     print(ffmpeg_cmd)
     subprocess.check_call(ffmpeg_cmd)
     assert(rx_ffmpeg.wait(test_time) == 0)
+    
+    test_file_params_json = subprocess.check_output(['ffprobe', '-i', 'test.mp3', '-hide_banner', '-show_format', '-show_streams', '-v', 'fatal', '-of', 'json']).decode()
+    test_file_params = json.loads(test_file_params_json)
+    assert(test_file_params['streams'][0]['codec_name'] == 'mp3')
+    assert(int(test_file_params['streams'][0]['sample_rate']) == 48000)
+    assert(int(test_file_params['streams'][0]['channels']) == 2)
+    assert(abs(float(test_file_params['streams'][0]['duration']) - test_time) < 0.1)
