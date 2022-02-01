16c096cbbd5b433a2d6fdc0be58a39c6c4e35596
Nathan Hui <ntlhui@gmail.com>
2022-01-31 19:43:16 -0800

    Updating animal flipper for BOR pipeline

--- SensorNode/sensor_nodes/animalFlipper.py
+++ SensorNode/sensor_nodes/animalFlipper.py
@@ -1,11 +1,21 @@
 import asyncio
+from dataclasses import dataclass
 import datetime as dt
 import os
-from typing import Dict, Tuple, Type, Union
+from typing import Dict, List, Tuple, Type, Union
 
+import schema
 from asm_protocol import codec
 from SensorNode import node
-from RaspiMotorHat.Raspi_MotorHAT import Raspi_MotorHAT as Raspi_MotorHAT
+
+MOTOR_HAT_ENABLED = False
+if os.path.exists('/sys/firmware/devicetree/base/model'):
+    with open('/sys/firmware/devicetree/base/model', 'r') as model:
+        pi_name = model.read()
+        if pi_name.find('Raspberry Pi') != -1:
+            MOTOR_HAT_ENABLED = True
+            from RaspiMotorHat.Raspi_MotorHAT import \
+                Raspi_MotorHAT as Raspi_MotorHAT
 
 class AnimalFlipper(node.SensorNodeBase):
     SENSOR_CLASS = 'ASM_AnimalFlipper'
@@ -13,14 +23,37 @@ class AnimalFlipper(node.SensorNodeBase):
     KEYS: Dict[str, Union[Type, Tuple[Type, Type]]] = {
         'motor': int,
         'home_direction': int,
-        'in_time_s': (int, float),
-        'in_steps': int,
-        'out_time_s': (int, float),
-        'out_steps': int,
         'motor_steps': int,
         'motor_speed': int,
         'data_file': str
     }
+    SCHEMA = schema.Schema(
+        {
+            'motor': schema.Or(1, 2),
+            'home_direction': int,
+            'motor_steps': int,
+            'motor_speed': schema.Or(int, float),
+            'home_direction': schema.Or(1, 2),
+            'data_file': str,
+            'loiter_time_s': schema.Or(int, float),
+            'out_threshold_steps': int,
+            'out_frame_steps': int,
+            'safe_steps': int,
+            'in_frame_steps': int,
+            'in_threshold_steps': int
+        }
+    )
+
+    IN_FRAME = 1
+    PARTIAL_FRAME = 2
+    OUT_FRAME = 3
+
+    @dataclass
+    class MotionPhase:
+        steps: int
+        direction: int
+        label: int
+        loiter: float
 
     def __init__(self, config_path: str = None):
         super().__init__(config_path=config_path)
@@ -28,65 +61,110 @@ class AnimalFlipper(node.SensorNodeBase):
             raise RuntimeError("No Aminal Flipper settings found")
         
         flipper_params = self._config_tree[self.SENSOR_CLASS]
-        for key, key_type in self.KEYS.items():
-            if key not in flipper_params:
-                raise RuntimeError(f'Key {key} not found in Flipper '
-                                   'settings')
-            if not isinstance(flipper_params[key], key_type):
-                raise RuntimeError(f'Expecting {key_type} for {self.SENSOR_CLASS}.'
-                                   f'{key}, got {type(flipper_params[key])} '
-                                   'instead!')
-        
-        self.motor_hat = Raspi_MotorHAT(0x6F)
-        self.stepper = self.motor_hat.getStepper(flipper_params['motor_steps'], flipper_params['motor'])
-        self.home_distance = flipper_params['motor_steps']
-        self.stepper.setSpeed(flipper_params['motor_speed'])
+        self.SCHEMA.validate(flipper_params)
+        if MOTOR_HAT_ENABLED:
+            self.motor_hat = Raspi_MotorHAT(0x6F)
+            self.stepper = self.motor_hat.getStepper(flipper_params['motor_steps'], flipper_params['motor'])
+            self.stepper.setSpeed(flipper_params['motor_speed'])
+        else:
+            self.__stepper_speed = 1 / flipper_params['motor_steps'] / flipper_params['motor_speed'] * 60.
+            self._log.warn("Motor Hat not enabled!")
 
-        self.in_time_s = flipper_params['in_time_s']
-        self.out_time_s = flipper_params['out_time_s']
-        self.in_steps = flipper_params['in_steps']
-        self.out_steps = flipper_params['out_steps']
+        self.home_distance = flipper_params['motor_steps']
         self.home_direction = flipper_params['home_direction']
-        if self.home_direction not in [1, 2]:
-            raise RuntimeError(f'Invalid home direction: {self.home_direction}')
         if self.home_direction == 1:
             self.in_direction = 1
             self.out_direction = 2
         else:
             self.in_direction = 2
             self.out_direction = 1
-        self.step_mode = Raspi_MotorHAT.MICROSTEP
+
+        self.__phases: List[AnimalFlipper.MotionPhase] = []
+        self.__phases.append(AnimalFlipper.MotionPhase(
+            steps=flipper_params['out_threshold_steps'],
+            direction=self.out_direction,
+            label=self.IN_FRAME,
+            loiter=0
+        ))
+        self.__phases.append(AnimalFlipper.MotionPhase(
+            steps=flipper_params['out_frame_steps'] - flipper_params['out_threshold_steps'],
+            direction=self.out_direction,
+            label=self.PARTIAL_FRAME,
+            loiter=0
+        ))
+        self.__phases.append(AnimalFlipper.MotionPhase(
+            steps=flipper_params['safe_steps'] - flipper_params['out_frame_steps'],
+            direction=self.out_direction,
+            label=self.OUT_FRAME,
+            loiter=flipper_params['loiter_time_s']
+        ))
+        self.__phases.append(AnimalFlipper.MotionPhase(
+            steps=flipper_params['safe_steps'] - flipper_params['in_frame_steps'],
+            direction=self.in_direction,
+            label=self.OUT_FRAME,
+            loiter=0
+        ))
+        self.__phases.append(AnimalFlipper.MotionPhase(
+            steps=flipper_params['in_frame_steps'] - flipper_params['in_threshold_steps'],
+            direction=self.in_direction,
+            label=self.PARTIAL_FRAME,
+            loiter=0
+        ))
+        self.__phases.append(AnimalFlipper.MotionPhase(
+            steps=flipper_params['in_threshold_steps'] + int(flipper_params['motor_steps'] / 4),
+            direction=self.in_direction,
+            label=self.IN_FRAME,
+            loiter=flipper_params['loiter_time_s']
+        ))
+        
+        if MOTOR_HAT_ENABLED:
+            self.step_mode = Raspi_MotorHAT.MICROSTEP
 
         self.data_file = flipper_params['data_file']
 
-    async def flipTask(self):
-        self.stepper.step(self.home_distance, self.home_direction, self.step_mode)
-        with open(self.data_file, 'a') as data_file:
-            while self.running:
-                self.stepper.step(self.out_steps, self.out_direction, self.step_mode)
-                now = dt.datetime.utcnow().isoformat()
-                data_file.write(f'{now}: out\n')
-                data_file.flush()
-                packet = codec.E4E_Flipper_Data(
-                    direction=codec.E4E_Flipper_Data.OUT,
-                    sourceUUID=self.uuid,
-                    destUUID=self.data_server_uuid)
-                await self.sendPacket(packet)
-                self.motor_hat.turnOffMotors()
-                await asyncio.sleep(self.out_time_s)
+    async def writeLabel(self, label: int):
+        now = dt.datetime.utcnow()
+        self._log.info(f'Recording label {label}')
 
-                self.stepper.step(self.in_steps, self.in_direction, self.step_mode)
-                now = dt.datetime.utcnow().isoformat()
-                data_file.write(f'{now}: in\n')
-                data_file.flush()
-                packet = codec.E4E_Flipper_Data(
-                    direction=codec.E4E_Flipper_Data.IN,
-                    sourceUUID=self.uuid,
-                    destUUID=self.data_server_uuid)
-                await self.sendPacket(packet)
-                self.motor_hat.turnOffMotors()
-                await asyncio.sleep(self.in_time_s)
+        with open(self.data_file, 'a') as data_file:
+            data_file.write(f'{now.isoformat()}: {label}\n')
+        
+        packet = codec.E4E_Data_Labels(
+            label=label, 
+            sourceUUID=self.uuid,
+            destUUID=self.data_server_uuid,
+            timestamp=now)
+        
+        await self.sendPacket(packet)
+
+    async def flipTask(self):
+        self._log.info("Slewing home")
+        if MOTOR_HAT_ENABLED:
+            self.stepper.step(self.home_distance, self.home_direction, self.step_mode)
+        else:
+            stepper_location = 0
+            await asyncio.sleep(self.home_distance * self.__stepper_speed)
+        self._log.info("Home")
+        while self.running:
+            for i, phase in enumerate(self.__phases):
+                self._log.info(f'Doing phase {i}')
+                await self.writeLabel(phase.label)
+                if MOTOR_HAT_ENABLED:
+                    self.stepper.step(phase.steps, phase.direction, self.step_mode)
+                else:
+                    self._log.info(f'Moving {phase.steps} in direction {phase.direction}')
+                    await asyncio.sleep(phase.steps * self.__stepper_speed)
+                    if phase.direction == 1:
+                        stepper_location -= phase.steps
+                    else:
+                        stepper_location += phase.steps
+                    if stepper_location < 0:
+                        stepper_location = 0
+                    self._log.info(f'Stepper now at {stepper_location}')
+                await asyncio.sleep(phase.loiter)
+            self._log.info('Home again')
+                
 
     async def setup(self):
         asyncio.create_task(self.flipTask())
-        return await super().setup()        
+        return await super().setup()
---
+++ docs/ASM_animalFlipper.md
@@ -0,0 +1,31 @@
+# Animal Flipper
+The overall objective of this device is to provide an Aye-Aye simulator.
+
+This device programmatically moves a stuffed lemur into and out of the box.
+
+We have designated 3 key labels:
+1. In Frame
+2. Partial Frame
+3. Out of Frame
+
+This system does have some troubles with accurate locating of the animal, so we shall make effort to always home the arm.
+
+The home position is the most "in" frame that the animal can be.  We can find this location by rotating in the home direction for at least `motor_steps`.
+
+We can then define a threshold value that determines the point at which the animal starts exiting the frame.  We define this to be `out_threshold_steps` from home.
+
+We can then define another threshold that determines the point at which the animal is completely out of the frame.  We define this to be at `out_frame_steps` from home.
+
+Note that during these, the arm is dragging the stuffed animal out of the box.  Thus, the behavior will be different when dragging the animal back into the box.
+
+We define another location `safe_steps` that places the stuffed animal fully out of frame and enables it to switch directions seamlessly.  This is the point at which the stuffed animal direction will switch.
+
+We define the `in_frame_steps` to be the point at which the animal starts to come back into the frame.
+
+We define the `in_threshold_steps` to be the point at which the animal is fully back into the frame.
+
+We also define a `loiter_time_s`, which is the time that the stuffed animal will loiter either at the home or safe positions.
+
+To ensure reliable locating, we will not disable the motors during any phase of this.  When we loiter at home, we will always drive into the home stop by a quarter rotation to ensure that we are fully at the home location.
+
+Labels are provided as the timestamp at the start of that label.  All data up to the next timestamp is that label.
