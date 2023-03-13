dbf102b4e19c6e6e10b61a66f832fbe6f26f3474
Nathan Hui <ntlhui@gmail.com>
2023-03-12 21:11:39 -0700

    PEP8

--- sensor_node/sensor_nodes/remote_sensor_node.py
+++ sensor_node/sensor_nodes/remote_sensor_node.py
@@ -1,3 +1,5 @@
+'''Remote Sensor Node
+'''
 import asyncio
 import os
 import pathlib
@@ -9,6 +11,19 @@ from sensor_node import node
 
 
 class RemoteSensorNode(node.SensorNodeBase):
+    """Remote Sensor Node
+
+    Args:
+        node (str): config_path
+
+    Raises:
+        RuntimeError: Key not found in config file
+        RuntimeError: Invalid value type
+        RuntimeError: IP Camera settings not found
+
+    Returns:
+        node.SensorNodeBase: Sensor Node
+    """
     SENSOR_CLASS = 'IP_Camera'
 
     IP_CAMERA_KEYS = {
@@ -31,35 +46,40 @@ class RemoteSensorNode(node.SensorNodeBase):
                 raise RuntimeError(f'Expecting {key_type} for IP_CAMERA.{key},'
                                    f' got {type(ip_camera_params[key])} '
                                    'instead!')
-            self._log.info(f'Discovered {key}: {ip_camera_params[key]}')
+            self._log.info('Discovered %s: %s', key, ip_camera_params[key])
         self.ip_camera_address = ip_camera_params['address']
-        assert(isinstance(self.ip_camera_address, str))
+        assert isinstance(self.ip_camera_address, str)
         self.ip_camera_user = ip_camera_params['user']
-        assert(isinstance(self.ip_camera_user, str))
+        assert isinstance(self.ip_camera_user, str)
         self.ip_camera_password = ip_camera_params['password']
-        assert(isinstance(self.ip_camera_password, str))
+        assert isinstance(self.ip_camera_password, str)
         self.ip_camera_port = ip_camera_params['port']
-        assert(isinstance(self.ip_camera_port, int))
+        assert isinstance(self.ip_camera_port, int)
 
         if os.getuid() == 0:
             self.ff_log_dir = pathlib.Path('var', 'log', 'ffmpeg_logs').absolute()
         else:
             # absolute() not necessary because of ASMSensorNode dir
             self.ff_log_dir = pathlib.Path(appdirs.user_log_dir('ASMSensorNode'), 'ffmpeg_logs')
-        pathlib.Path(self.ff_log_dir).mkdir(parents=True, exist_ok=True)        
+        pathlib.Path(self.ff_log_dir).mkdir(parents=True, exist_ok=True)
 
         self.registerPacketHandler(codec.E4E_START_RTP_RSP,
-                                   self.onRTPCommandResponse)
+                                   self.on_rtp_cmd_response)
 
     async def setup(self):
         command = codec.E4E_START_RTP_CMD(self.uuid, self.data_server_uuid, 1)
         await self.sendPacket(command)
         return await super().setup()
 
-    async def onRTPCommandResponse(self, packet: codec.binaryPacket):
-        assert(isinstance(packet, codec.E4E_START_RTP_RSP))
+    async def on_rtp_cmd_response(self, packet: codec.binaryPacket):
+        """On RTP Command response callback
+
+        Args:
+            packet (codec.binaryPacket): Binary packet
+        """
+        assert isinstance(packet, codec.E4E_START_RTP_RSP)
         endpoint_port = packet.port
-        
+
         ff_stats_path = pathlib.Path(self.ff_log_dir, "ffstats.log")
         ff_info_path = pathlib.Path(self.ff_log_dir, "ffinfo.log")
         split_script = "-m ASM_utils.ffmpeg.split_log"
@@ -72,8 +92,8 @@ class RemoteSensorNode(node.SensorNodeBase):
                )
         proc_out = asyncio.subprocess.PIPE
         proc_err = asyncio.subprocess.PIPE
-        self._log.info(f'Starting ffmpeg with command: {cmd}')
-        self._log.info(f"FFmpeg logging to: {self.ff_log_dir}")
+        self._log.info('Starting ffmpeg with command: %s', cmd)
+        self._log.info("FFmpeg logging to: %s", self.ff_log_dir)
         proc = await asyncio.create_subprocess_shell(cmd,
                                                      stdout=proc_out,
                                                      stderr=proc_err)
